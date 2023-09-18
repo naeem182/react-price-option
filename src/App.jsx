089699fbedded64a533e87efe17359ee84a692ed
naeem <naeemislamjr182@gmail.com>
2023-09-18 19:44:08 +0600

    daisy khalar nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import DaisyNav from './Components/Daisy_nav/DaisyNav'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
 
       <h1 className='text-7xl'>Vite + React</h1>
+      <div className=' container mx-auto'>
 
-      <div className="navbar bg-base-100">
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-          </button>
-        </div>
-        <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-        </div>
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-          </button>
-        </div>
+        <DaisyNav ></DaisyNav>
       </div>
     </>
   )
